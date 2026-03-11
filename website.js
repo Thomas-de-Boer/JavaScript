e1e687d69a9b72ac4210f0d0d21090b1cf5ddea5
Thomas <203590@novacollege.nl>
2026-03-11 14:19:14 +0100

    [UPDATE] assignment 5

--- website.js
+++ website.js
@@ -2,19 +2,73 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getpicture() {
-    return new Promise(await fetch("https://cataas.com/cat/funny"))
-}
-
 let bril = {
     "naam": "brilsmurf",
     "wc-bril": 0,
-    "zonnenbril": 1,
+    "zonnebril": 1,
     "kijkbril": 1,
     "lenssterkte": 0.3
 }
 
 isclicked = false
+
+let link = "https://cataas.com/cat"
+
+document.querySelector('#pokemon_form').addEventListener('submit', async function () {
+    event.preventDefault();
+
+    search = document.querySelector("#pokemon_search").value.toLowerCase()
+
+
+    async function getPokemon(search) {
+
+        const response = await fetch("config.php", {
+            method: "POST",
+            body: "pokemon=" + encodeURIComponent(search),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
+
+        const result = await response.text();
+        let json_result = JSON.parse(result);
+
+        document.querySelector("#result_name").innerText = json_result[0]['name'];
+        document.querySelector("#result_id").innerText = json_result[0]['id'];
+        document.querySelector("#result_species").innerText = json_result[0]['species'];
+        try {
+            const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+            const data = await pokeResponse.json();
+
+            document.querySelector("#result_image").src = data['sprites']['front_default'];
+        } catch (err) {
+            const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
+            const data = await pokeResponse.json();
+            document.querySelector("#result_image").src = data['sprites']['front_default'];
+        }
+    }
+
+    getPokemon(search);
+});
+
+
+document.querySelector('#cat_form').addEventListener('submit', function () {
+    event.preventDefault();
+
+    const search = document.querySelector("#cat_search").value.toLowerCase()
+
+    fetch("config.php", {
+        method: "POST",
+        body: "cat=" + encodeURIComponent(search),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+    })
+        .then(response => response.text())
+        .then(result => {
+            document.querySelector("#result_cat").innerHTML = result;
+    });
+});
 
 document.querySelector('#box1').style.backgroundColor = "orange";
 document.querySelector('#box2').style.backgroundColor = "turquoise";
@@ -31,8 +85,8 @@ window.addEventListener("load", () => {
     document.querySelector('#box2').addEventListener('click', () => {
         document.querySelector('#box2').style.backgroundColor = "turquoise";
         document.querySelector('#box2').innerText = ""
-        picture = getpicture()
-        document.querySelector('#box3').innerHTML = "<img src='https://cataas.com/cat/funny' alt='' width='100%' height='100%'/>";
+        fetch(link)
+        document.querySelector('#box3').innerHTML = "<img src='" + link + "' alt='' width='100%' height='100%'/>";
     });
 
     document.querySelector('#box3').addEventListener('click', () => {
@@ -44,16 +98,15 @@ window.addEventListener("load", () => {
     document.querySelector('#box4').addEventListener("click", () => {
         if (isclicked === false) {
             isclicked = true
-            document.querySelector('#box4').innerHTML = "<h2>naam: " + (bril.naam) + "</h2>" +
-                "<p>wc-bril: " + (bril["wc-bril"]) +
-                "<br>zonnenbril: " + (bril["zonnenbril"]) +
-                "<br>kijkbril: " + (bril["kijkbril"]) +
-                "<br>lenssterkte: " + (bril["lenssterkte"]) + "</p>";
+            document.querySelector('#box4').innerHTML = "<h2>Naam: " + (bril.naam) + "</h2>" +
+                "<p>Wc-bril: " + (bril["wc-bril"]) +
+                "<br>Zonnebril: " + (bril["zonnebril"]) +
+                "<br>Kijkbril: " + (bril["kijkbril"]) +
+                "<br>Lenssterkte: " + (bril["lenssterkte"]) + "</p>";
         } else {
             isclicked = false
             document.querySelector('#box4').innerHTML = "";
         }
-
     });
 });
 
